d7923ae0c673f8f9ab58525bc963e645f0845bd6
Rookie-cprime <syxsdhr@163.com>
2021-06-30 10:34:45 +0800

    1

---
+++ class_8/day_1/day_1.h
@@ -0,0 +1,12 @@
+#pragma once
+
+typedef int vector[10];
+typedef struct{
+    int  s[10];
+    int top;
+}stack;
+typedef struct{
+    int q[10];
+    int front,rear;
+}queue;
+
---
+++ class_8/day_1/test_0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <day_1.h>
+
+int insert(vector A,int n,int x){
+    int i = n-1;
+    while(true){
+        if(A[j]>x){
+
+int main()
+{
+}
+
